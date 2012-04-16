b62549a49f20cfbd37a816ca2de8daf79f72288f
Fabian <0inkane@googlemail.com>
2012-04-16 14:03:53 +0200

    switched from getopt to argparse, using python2

--- aur2ccr.py
+++ aur2ccr.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import sys
-import getopt # should I use some other import instead?
+import argparse
+
 
 def usage():
     print("usage info goes here")
 
 
 if __name__ == "__main__":
+
     usage()
